a410e68f171071f2ac2de0571aed2d5cf40bfac4
Eric Hostalery <hostalerye@gmail.com>
2013-04-02 12:06:53 -0400

    add helpers to make code easier to understand

--- app.js
+++ app.js
@@ -11,16 +11,11 @@ require('express-namespace')
 
 var configurations = require('./configurations.js')
 
-var db = require('mongojs').connect(configurations.db)
-
-
-
 var app = express()
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000)
   app.set('views', __dirname + '/views')
-  app.set('db', db)
   app.set('view engine', 'jade')
   app.use(express.favicon())
   app.use(express.logger('dev'))
